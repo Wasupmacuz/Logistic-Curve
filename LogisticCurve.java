d63e279b143404055094f1d0bcbbaaf5ef9a0503
Wasupmacuz <unicyclerider30@gmail.com>
2020-05-15 13:47:02 -0700

    Add files via upload

---
+++ LogisticCurve.java
@@ -0,0 +1,62 @@
+package tools;
+
+/**
+ * This class will evaluate a point on a specified logistic curve.<br>
+ * Optionally, a value for an x-intercept can be given so that the curve can go below zero.
+ * @author Grayson Hamari
+ * @version 05/15/2020
+ */
+public class LogisticCurve 
+{
+	float maxVal;
+	float slopeVal;
+	float midVal;
+	float yOffset = 0;
+	
+	/**
+	 * Create a logistic curve
+	 * @param maxVal The maximum value of the curve (technically never reaches this value)<br>
+	 * The limit of y as x approaches infinity equals L
+	 * @param slopeVal The slope of the curve at the inflection point
+	 * @param midVal The x value of the inflection point of the logistic function, where the slope is slopeVal
+	 * @since 05/13/2020
+	 */
+	public LogisticCurve(float maxVal, float slopeVal, float midVal)
+	{
+		this.maxVal = maxVal;
+		this.slopeVal = slopeVal;
+		this.midVal = midVal;
+	}
+
+	/**
+	 * Create a logistic curve
+	 * @param maxVal The maximum value of the curve (technically never reaches this value)<br>
+	 * The limit of y as x approaches infinity equals L
+	 * @param slopeVal The slope of the curve at the inflection point
+	 * @param midVal The x value of the inflection point of the logistic function, where the slope is slopeVal
+	 * @param xIntercept The value of x at which the user chooses the function to intercept the x-axis
+	 * @since 05/15/2020
+	 */
+	public LogisticCurve(float maxVal, float slopeVal, float midVal, float xIntercept)
+	{
+		// Setting the necessary variables before computing the y offset
+		this.maxVal = maxVal;
+		this.slopeVal = slopeVal;
+		this.midVal = midVal;
+		
+		yOffset = eval(xIntercept); // Computing the y offset now that the necessary variables are set
+		this.maxVal += yOffset; // reevaluating the maximum value with the new y offset
+	}
+
+	/**
+	 * Evaluates the logistic curve at a given x value
+	 * @param x The x value that the curve will be evaluated at
+	 * @return The y value of the curve at the given x value
+	 * @since 05/13/2020
+	 */
+	public float eval(float x)
+	{
+		float val = maxVal/(1 + (float)Math.pow(Math.E, -slopeVal*(x - midVal))) - yOffset;
+		return val;
+	}
+}
